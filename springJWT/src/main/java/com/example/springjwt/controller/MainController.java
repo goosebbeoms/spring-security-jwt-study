f30c0ca98a6f2ada9f0aa9b9d38dc901cca1ce83
goosebbeoms <seo980916@gmail.com>
2025-05-06 22:10:57 +0900

    일시적 세션 정보에서 사용자 정보 추출 구현

--- springJWT/src/main/java/com/example/springjwt/controller/MainController.java
+++ springJWT/src/main/java/com/example/springjwt/controller/MainController.java
@@ -1,8 +1,12 @@
 package com.example.springjwt.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
 
 @Controller
 @ResponseBody
@@ -10,6 +14,10 @@ public class MainController {
 
     @GetMapping("/")
     public String mainPage() {
-        return "main controller";
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        String authority = authorities.iterator().next().getAuthority();
+
+        return "main controller : " + name + "(role : " + authority + ")";
     }
 }
